669bb0b3ef2795aa9c1288d4db9a542a0ba44d4f
ebpereda.gbox.ncf.edu.ph <ely@example.com>
2025-12-05 09:35:48 -0800

    new

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <Link href="home"><Button variant="link">Don't click</Button></Link>
+        <Link href="login"><Button variant="link">Login</Button></Link>
     </div>
   );
 }
